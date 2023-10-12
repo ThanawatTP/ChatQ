26f73d43b8cf9aa29be2948c0d065ccec53c442a
Thanawat Thongpia <t.thanawat.thongpia@scbtechx.io>
2023-10-12 17:09:58 +0700

    fine-tune sentence embedding model

--- Description_base_linking.py
+++ Description_base_linking.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import sqlparse, time
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer, util, InputExample, losses
+from torch.utils.data import DataLoader
 
 class SchemaLinking():
     def __init__(self):
@@ -16,12 +17,22 @@ class SchemaLinking():
             sqlparse.sql.Parenthesis, sqlparse.sql.Operation, sqlparse.sql.Case
         }
     
-    def question_columns_scores(self,question):
+    def question_schema_columns_scores(self,question):
         q_emb = self.Sentence_transformer.encode(question)
         scores = np.array([float(util.cos_sim(q_emb, des)) for des in self.description_emb])
         return scores
     
-    def accuracy_of_vecs(self,choose_vector,expect_vector):
+    def add_cats_description(self,cat_columns):
+        data_df = pd.read_csv('src/pointx_fbs_rpt_dly.csv')
+        for col_name in cat_columns:
+            cats = set(data_df[col_name].values.tolist())
+            cats = [c for c in cats if not isinstance(c, float)]
+            add_description = ", ".join(cats)
+            idx = self.schema_description['Column'] == col_name, 'Description'
+            self.schema_description.loc[idx] += add_description
+        self.description_emb = [self.Sentence_transformer.encode(des) for des in self.schema_description['Description'].tolist()]
+
+    def accuracy_of_onehot(self,choose_vector,expect_vector):
         #compare accuracy of two one-hot vectors
         correct_matches = np.sum((choose_vector == 1) & (expect_vector == 1))
         total_elements = np.sum(expect_vector == 1)
@@ -32,7 +43,23 @@ class SchemaLinking():
 
         return accuracy
     
-    def choose_col_vec(self,columns):
+    def description_of_columns(self,column):
+        return self.schema_description[self.schema_description['Column'].isin(column)]['Description'].tolist()
+    
+    def fine_tune_model(self,questions_columns_list,epochs):
+        train_examples = []
+        for question, columns in questions_columns_list:
+            columns_description = " ".join(self.description_of_columns(columns))
+            data = [question, columns_description]
+            train_examples.append(InputExample(texts=data))
+
+        train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=64)
+        train_loss = losses.MultipleNegativesRankingLoss(model=self.Sentence_transformer)
+        self.Sentence_transformer.fit(train_objectives=[(train_dataloader, train_loss)],
+                                      epochs=epochs, show_progress_bar=False) 
+        print("Fine-tune model done!")
+    
+    def choose_col_onehot(self,columns):
         # return one hot vector of columns to choose
         col_vec = np.zeros(len(self.schema_columns))
         for col in columns:
@@ -48,7 +75,7 @@ class SchemaLinking():
         return col_vec
     
     
-    def q_c_similarity(self,question,column):
+    def ques_col_similarity(self,question,column):
         assert column in self.schema_columns, "Column not in schema"
         idx = self.schema_columns.index(column)
         col_emb = self.description_emb[idx]
@@ -57,13 +84,13 @@ class SchemaLinking():
         return score
 
 
-    def get_columns(self,question, threshold=0.2):
-        scores = self.question_columns_scores(question)
+    def get_columns_threshold(self,question, threshold=0.2):
+        scores = self.question_schema_columns_scores(question)
         columns = self.schema_description.iloc[np.where(scores >= threshold)]['Column'].tolist()
         return columns
 
     def similarity_threshold(self,question,selected_columns):
-        scores = self.question_columns_scores(question)
+        scores = self.question_schema_columns_scores(question)
         col_labels_index = self.schema_description[self.schema_description['Column'].isin(selected_columns)].index.tolist()
         col_labels_score = scores[col_labels_index]
         min_threshold = np.min(col_labels_score)
