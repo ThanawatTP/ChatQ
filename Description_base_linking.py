e3265d9309114dc03b703a52d67c64a72bbc46c7
Thanawat Thongpia <t.thanawat.thongpia@scbtechx.io>
2023-10-17 14:08:39 +0700

    restructure df, can add table

--- Description_base_linking.py
+++ Description_base_linking.py
@@ -1,56 +1,73 @@
 import pandas as pd
 import numpy as np
 import sqlparse, time
+# from transformers import AutoTokenizer, AutoModel
 from sentence_transformers import SentenceTransformer, util, InputExample, losses
 from torch.utils.data import DataLoader
 
 class SchemaLinking():
     def __init__(self):
         self.verbose = False
+        # self.tokenizer = AutoTokenizer.from_pretrained('models/all-MiniLM-L6-v2')
+        # self.model = AutoModel.from_pretrained('models/all-MiniLM-L6-v2')
         self.model = SentenceTransformer('models/all-MiniLM-L6-v2')
-        self.schema_description = pd.read_excel('src/New_query_Description.xlsx',header=1)[['Column','Description']].dropna().reset_index(drop=True)
-        self.description_emb = [self.sentence_embed(des) for des in self.schema_description['Description'].tolist()]
-        self.schema_columns = self.schema_description['Column'].tolist()
+        self.schema_description_df = pd.DataFrame(columns=['Table_name','Column','Description','Description_vector'])
+        self.schema_columns = self.schema_description_df['Column'].tolist()
+        self.set_schema()
         self.sql_extract_token_type = {
             sqlparse.sql.IdentifierList, sqlparse.sql.Where,
             sqlparse.sql.Having, sqlparse.sql.Comparison, sqlparse.sql.Function,
             sqlparse.sql.Parenthesis, sqlparse.sql.Operation, sqlparse.sql.Case
         }
 
-    def sentence_embed(self,sentence):
+    def set_vector_embedding(self):
+        self.schema_description_df['Description_vector'] = self.schema_description_df.apply(lambda row: self.sentence_embed(row['Description']), axis=1)
+
+    def set_schema(self,table_name:str="pointx_fbs_rpt_dly", schema_description_file_name:str='src/pointx_fbs_rpt_dly_description.csv'):
+        self.schema_temp = pd.read_csv(schema_description_file_name)
+        self.schema_temp['Table_name'] = table_name
+        self.schema_temp.insert(0, 'Table_name', self.schema_temp.pop('Table_name'))
+        self.schema_temp['Description_vector'] = self.schema_temp.apply(lambda row: self.sentence_embed(row['Description']), axis=1)
+        assert self.schema_temp.columns.tolist() == self.schema_description_df.columns.tolist(), "Schema columns not match"
+        self.schema_description_df = pd.concat([self.schema_description_df, self.schema_temp], ignore_index=True)
+        self.schema_columns = self.schema_description_df['Column'].tolist()
+
+    def sentence_embed(self,sentence:str):
         sentence_embeddings = self.model.encode(sentence)
         return sentence_embeddings 
     
-    def question_schema_columns_scores(self,question):
+    def question_schema_columns_scores(self,question:str):
         q_emb = self.sentence_embed(question)
-        scores = np.array([float(util.cos_sim(q_emb, des)) for des in self.description_emb])
+        scores = np.array([float(util.cos_sim(q_emb, des)) for des in self.schema_description_df['Description_vector'].tolist()])
         return scores
     
-    def add_cats_description(self,cat_columns):
+    def append_description_by_category(self,category_columns:list):
         data_df = pd.read_csv('src/pointx_fbs_rpt_dly.csv')
-        for col_name in cat_columns:
+        for col_name in category_columns:
             cats = set(data_df[col_name].values.tolist())
-            cats = [c for c in cats if not isinstance(c, float)]
+            cats = [c for c in cats if isinstance(c, str)]
             add_description = ", ".join(cats)
-            idx = self.schema_description['Column'] == col_name, 'Description'
-            self.schema_description.loc[idx] += add_description
-        self.description_emb = [self.sentence_embed(des) for des in self.schema_description['Description'].tolist()]
-
-    def accuracy_of_onehot(self,choose_vector,expect_vector):
-        #compare accuracy of two one-hot vectors
-        correct_matches = np.sum((choose_vector == 1) & (expect_vector == 1))
-        total_elements = np.sum(expect_vector == 1)
-        if total_elements == 0:
-            accuracy = 0.0  # Avoid division by zero
-        else:
-            accuracy = correct_matches / total_elements
-
-        return accuracy
+            idx = self.schema_description_df['Column'] == col_name, 'Description'
+            self.schema_description_df.loc[idx] += add_description
+        self.set_vector_embedding()
     
-    def description_of_columns(self,columns):
-        return self.schema_description[self.schema_description['Column'].isin(columns)]['Description'].tolist()
+    def description_of_columns(self,columns:list):
+        return self.schema_description_df[self.schema_description_df['Column'].isin(columns)]['Description'].tolist()
+
+
+    def ques_col_similarity(self,question:str,column:str):
+        assert column in self.schema_columns, "Column not in schema"
+        col_emb = self.schema_description_df[self.schema_description_df['Column'] == column]['Description_vector'].values[0]
+        q_emb = self.sentence_embed(question)
+        score = float(util.cos_sim(q_emb, col_emb))
+        return score
     
-    def fine_tune_model(self,questions_columns_list,epochs):
+    def get_columns_threshold(self,question:str, threshold=0.2):
+        scores = self.question_schema_columns_scores(question)
+        columns = self.schema_description_df.iloc[np.where(scores >= threshold)]['Column'].tolist()
+        return columns
+    
+    def fine_tune_model(self,questions_columns_list:list,epochs:int):
         train_examples = []
         for question, columns in questions_columns_list:
             columns_description = " ".join(self.description_of_columns(columns))
@@ -64,32 +81,31 @@ class SchemaLinking():
         
         print("Fine-tune model done!")
 
+    ##### Expirement testing #####
+    
     def choose_col_onehot(self,columns):
         # return one hot vector of columns to choose
+        assert self.schema_columns is not None, "Schema not set"
         col_vec = np.zeros(len(self.schema_columns))
         for col in columns:
             idx = self.schema_columns.index(col)
             col_vec[idx] = 1
         return col_vec
-
-    def ques_col_similarity(self,question,column):
-        assert column in self.schema_columns, "Column not in schema"
-        idx = self.schema_columns.index(column)
-        col_emb = self.description_emb[idx]
-        q_emb = self.sentence_embed(question)
-        score = float(util.cos_sim(q_emb, col_emb))
-        return score
     
-    def get_columns_threshold(self,question, threshold=0.2):
-        scores = self.question_schema_columns_scores(question)
-        columns = self.schema_description.iloc[np.where(scores >= threshold)]['Column'].tolist()
-        return columns
+    def accuracy_of_onehot(self,choose_onehot_vector,expect_onehot_vector):
+        #compare accuracy of two one-hot vectors
+        correct_matches = np.sum((choose_onehot_vector == 1) & (expect_onehot_vector == 1))
+        total_elements = np.sum(expect_onehot_vector == 1)
+        if total_elements == 0:
+            accuracy = 0.0  # Avoid division by zero
+        else:
+            accuracy = correct_matches / total_elements
 
-    ##### Expirement testing #####
+        return accuracy
 
     def similarity_threshold(self,question,selected_columns):
         scores = self.question_schema_columns_scores(question)
-        col_labels_index = self.schema_description[self.schema_description['Column'].isin(selected_columns)].index.tolist()
+        col_labels_index = self.schema_description_df[self.schema_description_df['Column'].isin(selected_columns)].index.tolist()
         col_labels_score = scores[col_labels_index]
         min_threshold = np.min(col_labels_score)
         n_columns = len(self.get_columns_threshold(question,min_threshold))
