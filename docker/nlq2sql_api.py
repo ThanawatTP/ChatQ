3e73f5610e7ee12fd4a83d882757dff185f30c8e
Thanawat Thongpia <t.thanawat.thongpia@scbtechx.io>
2023-10-26 16:50:57 +0700

    wrap with fast api

--- docker/nlq2sql_api.py
+++ docker/nlq2sql_api.py
@@ -1,6 +1,7 @@
 import time, argparse, json, warnings, yaml
 from Description_base_linking import SchemaLinking
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from fastapi import Body, FastAPI
 
 def column_index(use_cols,ls_all_col_w_dtype):
     col_ind = {}
@@ -29,9 +30,21 @@ def gen_sql(prompt):
     resp = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
     return resp
 
-def pipeline_process(nlq, threshold):
 
-    nlq = json.loads(nlq)
+with open('nlq2sql_parameters.yaml', 'r') as yaml_file:
+        params = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+warnings.filterwarnings("ignore")
+schema_link = SchemaLinking()
+tokenizer = AutoTokenizer.from_pretrained(params['nsql_model_path'])
+model = AutoModelForCausalLM.from_pretrained(params['nsql_model_path'])
+verbose = True
+app = FastAPI()
+
+@app.post("/nlq")
+async def pipeline_process(nlq=Body()):
+
+    threshold = 0.2
     question = nlq['input']['text']
 
     # schema column : data type
@@ -70,7 +83,6 @@ def pipeline_process(nlq, threshold):
             "total_tokens": 8
         }
     }
-    print(json.dumps(output, indent=4))
 
     if verbose:
         print("=== QUESTION ===")
@@ -89,24 +101,24 @@ def pipeline_process(nlq, threshold):
     
 
 
-if __name__ == '__main__':
-    # start = time.time()
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--nlq', required=True, help="Input question")
-    arg_parser.add_argument('--threshold', type=float, default=0.2, help="Minimum sentence similarity threshold score")
-    arg_parser.add_argument('--param_yaml_path', type=str, default='nlq2sql_parameters.yaml', help="Parameter config file")
-    arg_parser.add_argument('--verbose', type=bool, default=True)
-    args = arg_parser.parse_args()
+# if __name__ == '__main__':
+#     # start = time.time()
+#     arg_parser = argparse.ArgumentParser()
+#     arg_parser.add_argument('--nlq', required=True, help="Input question")
+#     arg_parser.add_argument('--threshold', type=float, default=0.2, help="Minimum sentence similarity threshold score")
+#     arg_parser.add_argument('--param_yaml_path', type=str, default='nlq2sql_parameters.yaml', help="Parameter config file")
+#     arg_parser.add_argument('--verbose', type=bool, default=True)
+#     args = arg_parser.parse_args()
 
-    warnings.filterwarnings("ignore")
-    with open(args.param_yaml_path, 'r') as yaml_file:
-        params = yaml.load(yaml_file, Loader=yaml.FullLoader)
+#     warnings.filterwarnings("ignore")
+#     with open(args.param_yaml_path, 'r') as yaml_file:
+#         params = yaml.load(yaml_file, Loader=yaml.FullLoader)
     
-    schema_link = SchemaLinking()
-    tokenizer = AutoTokenizer.from_pretrained(params['nsql_model_path'])
-    model = AutoModelForCausalLM.from_pretrained(params['nsql_model_path'])
-    verbose = True
-    pipeline_process(args.nlq, args.threshold)
-    # print(f'e2e processing time = {time.time()-start:.2f} seconds')
+#     schema_link = SchemaLinking()
+#     tokenizer = AutoTokenizer.from_pretrained(params['nsql_model_path'])
+#     model = AutoModelForCausalLM.from_pretrained(params['nsql_model_path'])
+#     verbose = True
+#     pipeline_process(args.nlq, args.threshold)
+#     # print(f'e2e processing time = {time.time()-start:.2f} seconds')
 
 
